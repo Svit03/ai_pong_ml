34a2eed7ac3757b697334fe74f8850555244e04b
Vitaly <vitas.igymnov@gmail.com>
2026-04-21 15:25:11 +0800

    feat: add moving ball on canvas (60 FPS animation)

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,30 @@
-console.log(" game.js подключён ");
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+let ballX = 400;
+let ballY = 300;
+let ballSpeedX = 5;
+let ballSpeedY = 3;
+let ballRadius = 10;
+
+function draw() {
+    ctx.fillStyle = '#111';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+    ctx.fill();
+    
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if (ballX + ballRadius > canvas.width || ballX - ballRadius < 0) {
+        ballSpeedX = -ballSpeedX;
+    }
+    if (ballY + ballRadius > canvas.height || ballY - ballRadius < 0) {
+        ballSpeedY = -ballSpeedY;
+    }
+}
+
+setInterval(draw, 1000 / 60);
